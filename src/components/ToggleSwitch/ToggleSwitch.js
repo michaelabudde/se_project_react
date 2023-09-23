d3105d6b0864e4db35018cde7f64c90bdd62e147
Michaela Budde <michaelabudde@Michaelas-MacBook-Pro.local>
2023-09-23 13:18:03 -0600

    toggle switch

--- src/components/ToggleSwitch/ToggleSwitch.js
+++ src/components/ToggleSwitch/ToggleSwitch.js
@@ -1,6 +1,21 @@
 import React from "react";
+import "./ToggleSwitch.css";
 const ToggleSwitch = () => {
   console.log("Toggle");
-  return <div> Toggle Switch</div>;
+  const handleChange = (e) => {
+    console.log(e);
+  };
+  return (
+    <label className="switch">
+      <input
+        type="checkbox"
+        className="switch__box"
+        onChange={handleChange}
+      ></input>
+      <span></span>
+      <p>F</p>
+      <p>C</p>
+    </label>
+  );
 };
 export default ToggleSwitch;
